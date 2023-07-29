3343221776fa795c519c2268dc4c5cdbfe72e44f
shishouheng <1055067459@qq.com>
2023-07-29 11:47:23 +0800

    Monster show

---
+++ note/UI实现怪物展示/Monster.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    private SkinnedMeshRenderer skinnedMeshRenderer;
+    public Texture[] skinTextures;
+    public Animation ani;
+    public Transform[] weapons;
+    public Transform wpHand;
+    private void Awake()
+    {
+        skinnedMeshRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
+        ani = GetComponent<Animation>();
+    }
+    public void ChangeSkin(int index)
+    {
+        skinnedMeshRenderer.material.mainTexture = skinTextures[index];
+    }
+    public void ChangeAni(string aniName)
+    {
+        ani.Play(aniName);
+    }
+    public void ChangeWeapon(int index)
+    {
+        if(wpHand.childCount>=2)
+        {
+            DestroyImmediate(wpHand.GetChild(1).gameObject);
+        }
+        GameObject mWeapon=Instantiate<GameObject>(weapons[index].gameObject, wpHand.position, wpHand.rotation);
+        mWeapon.transform.SetParent(wpHand);
+    }
+}
---
+++ note/UI实现怪物展示/MonsterBasePanel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterBasePanel : MonoBehaviour
+{
+    public GameObject[] monsters;
+    private GameObject lastMonsters;//当前在木板上展示的怪物
+    private void Start()
+    {
+        lastMonsters = monsters[0];
+    }
+    public void OnClickIcon(int index)
+    {
+        //当前下标对应的怪物激活
+        //monsters[index].SetActive(true);
+        ////其他怪物隐藏
+        //for(int i=0;i<monsters.Length;i++)
+        //{
+        //    if (i != index)
+        //        monsters[i].SetActive(false);
+        //}
+
+
+        //当前展示的怪物隐藏
+        lastMonsters.SetActive(false);
+        //激活当前怪物
+        monsters[index].SetActive(true);
+        //更新当前怪物
+        lastMonsters = monsters[index];
+    }
+    public void OnClickChangeSkin(int index)
+    {
+        lastMonsters.GetComponent<Monster>().ChangeSkin(index);
+    }
+    public void OnClickChangeAni(string aniName)
+    {
+        lastMonsters.GetComponent<Monster>().ChangeAni(aniName);
+    }
+    public void MonsterRotate()
+    {
+        if (Input.GetMouseButton(1))
+            lastMonsters.transform.rotation *= Quaternion.Euler(0, -Input.GetAxis("Mouse X"), 0);
+
+    }
+    private void Update()
+    {
+        MonsterRotate();
+    }
+    public void OnClickMonsterWeapon(int index)
+    {
+        lastMonsters.GetComponent<Monster>().ChangeWeapon(index);
+    }
+}
